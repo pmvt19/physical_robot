6f3afad152864c7a2bb71a72ff1d49d273d08b1c
ptelag2 <ptelag2@illinois.edu>
2025-09-30 22:48:48 -0700

    Interface can move based on precise motion requirements and can update a state estimatation given a motion

--- robot_interface.py
+++ robot_interface.py
@@ -6,13 +6,16 @@ class RobotInterface():
     def __init__(self, controller):
         self.controller : DynamixelController = controller
 
-        self.linear_velocity = 20
+        self.linear_velocity = 10
 
         self.controller.set_torque(1, TORQUE_DISABLE)
         self.controller.set_torque(2, TORQUE_DISABLE)
 
-        self.controller.set_operating_mode(1, VELOCITY_CONTROL_MODE)
-        self.controller.set_operating_mode(2, VELOCITY_CONTROL_MODE)
+        # self.controller.set_operating_mode(1, VELOCITY_CONTROL_MODE)
+        # self.controller.set_operating_mode(2, VELOCITY_CONTROL_MODE)
+
+        self.controller.set_operating_mode(1, EXTENDED_POSITION_CONTROL_MODE)
+        self.controller.set_operating_mode(2, EXTENDED_POSITION_CONTROL_MODE)
 
         self.controller.set_torque(1, TORQUE_ENABLE)
         self.controller.set_torque(2, TORQUE_ENABLE)
@@ -22,6 +25,22 @@ class RobotInterface():
 
         self.before_motion_positions = []
         self.after_motion_positions = []
+
+    def set_torque(self, torque):
+        self.controller.set_torque(1, torque)
+        self.controller.set_torque(2, torque)
+
+    def set_mode(self, mode):
+        raise NotImplementedError
+    
+    def set_profile_velocity(self, velocity=100):
+        self.controller.set_profile_velocity(id=1, velocity=velocity)
+        self.controller.set_profile_velocity(id=2, velocity=velocity)
+
+    def get_motor_velocity(self):
+        v1 = self.controller.get_velocity(id=1)
+        v2 = self.controller.get_velocity(id=1)
+        return np.array([v1, v2])
         
     def move_forward(self):
         self.controller.set_velocity(id=1, velocity_rpm=self.linear_velocity)
@@ -81,6 +100,64 @@ class RobotInterface():
         final_motor_pos = ri.get_motor_positions()
 
         return self.compute_linear_motion(init_motor_pos, final_motor_pos)
+    
+    def move_dist(self, mm=100):
+        self.set_torque(TORQUE_DISABLE)
+        init_motor_pos = ri.get_motor_positions()
+        self.set_torque(TORQUE_ENABLE)
+        print(f"Initial Motor Positions: {init_motor_pos}")
+
+        cir = self.r * 2 * np.pi
+
+        required_revs = mm / cir
+
+        req_pulse = int(required_revs * 4096)
+
+        desired_motor_pos_1 = init_motor_pos[0] + req_pulse
+        desired_motor_pos_2 = init_motor_pos[1] - req_pulse
+        print(f"Desired Motor Positions: {[desired_motor_pos_1, desired_motor_pos_2]}")
+
+        self.controller.set_position(id=1, position=desired_motor_pos_1)
+        self.controller.set_position(id=2, position=desired_motor_pos_2)
+        # time.sleep(5)
+        # self.set_torque(TORQUE_DISABLE)
+
+        while np.sum(self.get_motor_velocity()) > 0:
+            continue
+        
+        final_motor_pos = ri.get_motor_positions()
+        print(f"Final Motor Positions: {final_motor_pos}")
+        return self.compute_linear_motion(init_motor_pos, final_motor_pos)
+    
+    def rotate_rad(self, rad=np.pi/2):
+        init_motor_pos = ri.get_motor_positions()
+        print(f"Initial Motor Positions: {init_motor_pos}")
+
+        cir = self.r * 2 * np.pi
+        body_cir = 2 * np.pi * (self.L / 2)
+
+        rotation_percentage = rad / (2 * np.pi)
+
+        wheel_travel_dist = body_cir * rotation_percentage
+
+        required_revs = wheel_travel_dist / cir
+
+        req_pulse = int(required_revs * 4096)
+
+        desired_motor_pos_1 = init_motor_pos[0] + req_pulse
+        desired_motor_pos_2 = init_motor_pos[1] + req_pulse
+        print(f"Desired Motor Positions: {[desired_motor_pos_1, desired_motor_pos_2]}")
+
+        self.controller.set_position(id=1, position=desired_motor_pos_1)
+        self.controller.set_position(id=2, position=desired_motor_pos_2)
+
+        while np.sum(self.get_motor_velocity()) > 0:
+            continue
+
+        final_motor_pos = ri.get_motor_positions()
+        print(f"Final Motor Positions: {final_motor_pos}")
+        return self.compute_rotation_motion(init_motor_pos, final_motor_pos)
+    
 
     def compute_linear_motion(self, init_mp, final_mp):
         init_mp = np.array(init_mp)
@@ -115,12 +192,34 @@ class RobotInterface():
     def get_motor_positions(self):
         motor_id_1_pos = self.controller.get_position(id=1)
         motor_id_2_pos = self.controller.get_position(id=2)
-        return [motor_id_1_pos, motor_id_2_pos]
+        return np.array([motor_id_1_pos, motor_id_2_pos])
 
 
 
     # Wheel Diameter (r) is 66.5 mm
     # Wheel Base (L) is 105*2 mm = 210 mm
+
+    def predict_state(self, state, motion, motion_type='linear'):
+        motion = np.abs(motion) # This should be directional and not a magnitude
+        # avg_motion = np.mean(motion)
+        avg_motion = np.min(motion)
+
+        x, y, theta = state
+
+        if motion_type == 'linear':
+
+            direction_vector = np.array([np.cos(theta), np.sin(theta), 0.0])
+            dx_state = direction_vector * avg_motion
+            updated_state = state + dx_state
+
+        elif motion_type == 'angular':
+            dx_state = np.array([0.0, 0.0, avg_motion])
+            updated_state = state + dx_state
+        else:
+            raise NotImplementedError
+
+        return updated_state
+        
 
 def rotational_main(controller, ri):
     init_motor_pos = ri.get_motor_positions()
@@ -179,22 +278,48 @@ if __name__ == '__main__':
     # linear_main(controller, ri)
     # rotational_main(controller, ri)
 
-    linear_movement_1 = ri.move_mm(200)
-    input()
-    rotation_1 = ri.rotate_deg(deg=np.pi/2)
-    input()
-    linear_movement_2 = ri.move_mm(450)
-    input()
-    rotation_2 = ri.rotate_deg(deg=np.pi/2)
-    input()
-    linear_movement_3 = ri.move_mm(200)
-    input()
+    # Real Life Env Testing
+    # linear_movement_1 = ri.move_mm(200)
+    # input()
+    # rotation_1 = ri.rotate_deg(deg=np.pi/2)
+    # input()
+    # linear_movement_2 = ri.move_mm(450)
+    # input()
+    # rotation_2 = ri.rotate_deg(deg=np.pi/2)
+    # input()
+    # linear_movement_3 = ri.move_mm(200)
+    # input()
 
-    print(linear_movement_1)
-    print(rotation_1)
-    print(linear_movement_2)
-    print(rotation_2)
-    print(linear_movement_3)
+    # print(linear_movement_1)
+    # print(rotation_1)
+    # print(linear_movement_2)
+    # print(rotation_2)
+    # print(linear_movement_3)
+
+    # Testing Computed Motor Position for Distances (Rather than Timing)
+    # ri.set_profile_velocity()
+    # move = ri.move_dist(150)
+    # move = ri.rotate_rad()
+    # print(move)
+
+    # Testing Motion and Predicted State
+    ri.set_profile_velocity()
+    state = np.array([0.0, 0.0, 0.0])
+    print(f"State: {state}")
+    m1 = ri.move_dist(300)
+    print(f"Motion: {m1}")
+    state = ri.predict_state(state, m1, 'linear')
+    print(f"State: {state}")
+    m2 = ri.rotate_rad(np.pi/4)
+    print(f"Motion: {m2}")
+    state = ri.predict_state(state, m2, 'angular')
+    print(f"State: {state}")
+    m3 = ri.move_dist(400)
+    print(f"Motion: {m3}")
+    state = ri.predict_state(state, m3, 'linear')
+    print(f"State: {state}")
+
+
     # st = time.time()
     # ri.move_forward()
     # while (time.time() - st) < 5:
