bcf14965ed97f58803cda7046c85f641a2be5b53
pmvt19 <praval.telagi@gmail.com>
2025-10-14 23:36:06 -0700

    Added particle filter probability calculation

--- particle_filter.py
+++ particle_filter.py
@@ -1,32 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import ndimage
+from scipy import ndimage, stats
+from map import Map
 
 from simulate_lidar import SimulatedLidar
 
 from utils import pairwise_dists
-from test_utils import generate_fake_map
+from test_utils import generate_fake_map, load_saved_map
 
 class ParticleFilter():
-    def __init__(self, map_obj):
-        self.map = map_obj
+    def __init__(self, map_obj, scale_factor=10):
+        self.map : Map = map_obj
+        self.scale_factor = scale_factor
+        print("WARNING: GUESS ON THE SCALE FACTOR!!")
+
+        self.normal_distribution = stats.norm(loc=0, scale=self.scale_factor)
 
     def _compute_dist_map(self):
         inverse_map = 1 - self.map.map
         self.dist_map = ndimage.distance_transform_edt(inverse_map)
 
+    def _batch_get_simulated_lidar(self):
+        raise NotImplementedError
+
+    def _batch_get_probabilities(self):
+        raise NotImplementedError
+
     # TODO: Change function name - Done?
-    def batch_get_measurement_update(self, states, scan_actual):
+    def batch_get_measurement_update(self, states, scan_actual, tmp_points=None):
         # States: (N, 3)
         # Scan_Actual: (M, 2)
         # Ideally, scan_actual[i] is just (angle, dist)
+
+        ### ---- Get Batch Simulated Lidar ---- ###
         N, d = states.shape
 
         angles = scan_actual[:, 0] # (M,)
         point_dists = scan_actual[:, 1] # (M,)
 
         state_headings = states[:, 2] # (N,)
-        offset_angles = angles.reshape(-1, 1) + state_headings.reshape(1, -1) # (M, 1) + (1, N) = (M, N)
+        offset_angles = angles.reshape(-1, 1) - state_headings.reshape(1, -1) # (M, 1) + (1, N) = (M, N)
+        print(np.rad2deg(angles % (2*np.pi)), state_headings)
+        # exit()
 
         coses = np.cos(offset_angles) # (M, N)
         sines = np.sin(offset_angles) # (M, N)
@@ -35,7 +50,24 @@ class ParticleFilter():
         origin_centered_points = vecs * point_dists.reshape(-1, 1, 1) # (M, N, 2) * (M,) = (M, N, 2) (MIGHT NEED TO RESHAPE point_dists)
         batch_simulated_lidar_readings = origin_centered_points + states[:, :2].reshape(1, N, -1) # (M, N, 2) + (1, N, 2) = (M, N, 2) (Probably need to transform states a bit)
         batch_simulated_lidar_readings = batch_simulated_lidar_readings.transpose(1, 0, 2) # Transpose the Matrix to be (N, M, 2) {I think this makes more sense, but I already implemented this function...}
-        return batch_simulated_lidar_readings
+
+        plt.scatter(batch_simulated_lidar_readings[0, :, 0], batch_simulated_lidar_readings[0, :, 1], label='sim lidar')
+        plt.scatter(tmp_points[:, 0], tmp_points[:, 1], label='scanned lidar')
+        self.map.visualize_points(plt.gca())
+        plt.legend()
+        plt.show()
+        ### ---- Get Batch Simulated Lidar ---- ###
+
+        ### ---- Get Probabilities ---- ###
+        flattened_batch_simulated_lidar_readings = batch_simulated_lidar_readings.reshape(-1, 2) # (N, M, 2) -> (N*M, 2)
+        flattened_batch_grid_coords = self.map.batch_world_to_grid_coords(flattened_batch_simulated_lidar_readings) # (N*M, 2)
+        flattened_batch_dists = self.dist_map[flattened_batch_grid_coords[:, 0], flattened_batch_grid_coords[:, 1]] # (N*M,)
+        print(flattened_batch_dists)
+        flattened_batch_probs = self.normal_distribution.pdf(flattened_batch_dists) # (N*M,)
+        batch_probs = flattened_batch_probs.reshape(N, -1) # (N, M)
+        ### ---- Get Probabilities ---- ###
+
+        return batch_probs, batch_simulated_lidar_readings
 
 
     def visualize_dist_map(self, ax):
@@ -47,15 +79,21 @@ class ParticleFilter():
 
 if __name__ == '__main__':
     mymap = generate_fake_map()
+    # mymap = load_saved_map()
+
+    
 
     state = np.array([-1000.0, 2500.0, np.pi/2])
 
     sl = SimulatedLidar(mymap, 100, 10000)
-    _, _, _, angles, r_dists = sl.simulate_lidar(loc=state)
+    translated_rotated_points, line_segment_eps, map_points, angles, r_dists, unrotated_points, r_angles = sl.simulate_lidar(loc=state)
     # Format Lidar Readings:
+    print(translated_rotated_points.shape, line_segment_eps.shape, map_points.shape)
     print(angles.shape, r_dists.shape)
     scan = np.stack((angles, r_dists), axis=1)
-    print(scan.shape)
+    scan_v2 = np.stack((r_angles, r_dists), axis=1)
+    print("Scan Shapes")
+    print(scan.shape, scan_v2.shape)
 
 
     pf = ParticleFilter(mymap)
@@ -65,8 +103,10 @@ if __name__ == '__main__':
     pf.visualize_map(ax1)
     pf.visualize_dist_map(ax2)
     plt.show()
-
-    pf.batch_get_measurement_update(state.reshape(-1, 3), scan)
+    
+    
+    batch_probs, batch_simulated_lidar_readings = pf.batch_get_measurement_update(state.reshape(-1, 3), scan_v2, translated_rotated_points)
+    print(batch_probs)
 
 
     
