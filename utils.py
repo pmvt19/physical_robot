24df1252d556c281ba6b7082dcfc7ec19cb13f22
pmvt19 <praval.telagi@gmail.com>
2025-10-14 19:28:01 -0700

    Added pairwise_dists to utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import logging
+import numpy as np
 
 def register_logger(logger_name, log_filename, level=logging.INFO, std_err=False):
     logger = logging.getLogger(logger_name)
@@ -10,3 +11,7 @@ def register_logger(logger_name, log_filename, level=logging.INFO, std_err=False
     if std_err:
         logger.addHandler(logging.StreamHandler())
     return logger
+
+def pairwise_dists(points1, points2):
+    dists = np.sqrt(np.sum(points1**2, axis=1, keepdims=True) + np.sum(points2**2, axis=1, keepdims=True).T + (-2 * (points1 @ points2.T)))
+    return dists
