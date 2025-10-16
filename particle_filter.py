6b2437ec2649e58c0198da723f0661ed433723b4
pmvt19 <praval.telagi@gmail.com>
2025-10-15 20:23:04 -0700

    Particle Filter Running

--- particle_filter.py
+++ particle_filter.py
@@ -29,7 +29,10 @@ class ParticleFilter():
     
     def generate_initial_particles(self, num_particles):
         print("WARNING: GENERATE INNITIAL PARTICLES NOT WORKING AS INTENDED!!!")
-        self.particles = np.random.uniform(size=(num_particles, 3))
+        self.particles = np.random.uniform(low=np.array([-2000, -2000, 0]), high=np.array([2000, 3000, 2*np.pi]), size=(num_particles, 3))
+        weights = 1 / num_particles
+        batch_uniform_weights = np.ones(num_particles) * weights
+        self.particles = np.concatenate((self.particles, batch_uniform_weights.reshape(-1, 1)), axis=1)
 
     # TODO: Change function name - Done?
     def batch_get_measurement_update(self, states, scan_actual, tmp_points=None):
@@ -60,11 +63,11 @@ class ParticleFilter():
         batch_simulated_lidar_readings = origin_centered_points + states[:, :2].reshape(1, N, -1) # (M, N, 2) + (1, N, 2) = (M, N, 2) (Probably need to transform states a bit)
         batch_simulated_lidar_readings = batch_simulated_lidar_readings.transpose(1, 0, 2) # Transpose the Matrix to be (N, M, 2) {I think this makes more sense, but I already implemented this function...}
 
-        plt.scatter(batch_simulated_lidar_readings[0, :, 0], batch_simulated_lidar_readings[0, :, 1], label='sim lidar')
-        plt.scatter(tmp_points[:, 0], tmp_points[:, 1], label='scanned lidar')
-        self.map.visualize_points(plt.gca())
-        plt.legend()
-        plt.show()
+        # plt.scatter(batch_simulated_lidar_readings[0, :, 0], batch_simulated_lidar_readings[0, :, 1], label='sim lidar')
+        # plt.scatter(tmp_points[:, 0], tmp_points[:, 1], label='scanned lidar')
+        # self.map.visualize_points(plt.gca())
+        # plt.legend()
+        # plt.show()
         ### ---- Get Batch Simulated Lidar ---- ###
 
         ### ---- Get Probabilities ---- ###
@@ -83,14 +86,15 @@ class ParticleFilter():
         ### ---- Get Particle Weights ---- ###
 
         # -- Should have an Underflow Problem -- #
-        batch_weights_unnormalized = np.prod(batch_probs, axis=0).squeeze()
-        batch_weights_normalized = batch_weights_unnormalized / np.sum(batch_weights_unnormalized)
+        # batch_weights_unnormalized = np.prod(batch_probs, axis=0).squeeze()
+        # batch_weights_normalized = batch_weights_unnormalized / np.sum(batch_weights_unnormalized)
         # -- Should have an Underflow Problem -- #
 
 
         # -- Should avoid Underflow Problem with Log Scaling -- #
+        print("batch probs shape", batch_probs.shape)
         batch_log_probs = np.log(batch_probs)
-        batch_log_weights = np.sum(batch_log_probs, axis=0).squeeze()
+        batch_log_weights = np.sum(batch_log_probs, axis=1).squeeze()
         batch_rescaled_log_weights = batch_log_weights - np.max(batch_log_weights)
         batch_unnormalized_weights = np.exp(batch_rescaled_log_weights)
         batch_normalized_weights = batch_unnormalized_weights / np.sum(batch_unnormalized_weights)
@@ -105,23 +109,19 @@ class ParticleFilter():
     def _noise_injection(self, particles):
 
         mu_noise = np.array([0.0, 0.0, 0.0])
-        Q_noise = np.diag([3.0, 3.0, 0.2])
+        Q_noise = np.array([3.0, 3.0, 0.2])
 
-        print("DEBUGGING:")
-        print(Q_noise.shape)
-        print("DEBUGGING:")
-
-        noise = np.random.normal(loc=mu_noise, scale=Q_noise)
+        noise = np.random.normal(loc=mu_noise, scale=Q_noise, size=(len(particles), 3))
         particles[:, :3] = particles[:, :3] + noise
         return particles
     
     def _delta_noise_injection(self, motion_delta):
         num_particles = len(self.particles)
-        delta_noise = np.random.normal(loc=np.array([0.0, 0.0, 0.0], scale=np.array([0.1, 0.1, 0.1], size=(num_particles, 3))))
+        delta_noise = np.random.normal(loc=np.array([0.0, 0.0, 0.0]), scale=np.array([0.1, 0.1, 0.1]), size=(num_particles, 3))
         noisy_motion_delta = motion_delta + delta_noise
         return noisy_motion_delta
 
-    def resample(self, num_particles_to_sample=1):
+    def resample(self, num_particles_to_sample=1000):
         # Resampling Implicitly Handles Killing Low-Weight Particles
         num_particles = len(self.particles)
         particle_weights = self.particles[:, 3]
@@ -144,7 +144,7 @@ class ParticleFilter():
             orientation_states = self.particles[:, 2]
             particle_weights = self.particles[:, 3]
 
-            mean_position = np.sum(particle_weights * positional_states, axis=0)
+            mean_position = np.sum(particle_weights.reshape(-1, 1) * positional_states, axis=0)
 
             coses = np.cos(orientation_states)
             sines = np.sin(orientation_states)
@@ -173,14 +173,17 @@ class ParticleFilter():
         
     def update_particle_weights(self, scan):
         _, _, batch_normalized_weights = self.batch_get_measurement_update(self.particles[:, :3], scan)
+        print(self.particles.shape, batch_normalized_weights.shape)
         self.particles[:, 3] = batch_normalized_weights
 
     def step(self, motion_delta, scan, estimate_method='MLE'):
-        self.particles = self.get_updated_particles(motion_delta) # TODO: IMPLEMENT THIS
-        self.update_particle_weights(scan) # TODO: IMPLEMENT THIS
-        state_estimate = self.get_state_estimate(method=estimate_method) # IMPLEMENTED
+        self.particles = self.get_updated_particles(motion_delta)
+        self.update_particle_weights(scan)
+        state_estimate = self.get_state_estimate(method=estimate_method)
         self.resample()
         return state_estimate
+    
+    ### --- Visualization Functions --- ###
 
     def visualize_dist_map(self, ax):
         ax.imshow(np.rot90(self.dist_map))
@@ -211,8 +214,6 @@ if __name__ == '__main__':
     mymap = generate_fake_map()
     # mymap = load_saved_map()
 
-    
-
     state = np.array([-1000.0, 2500.0, np.pi/2])
 
     ### ---- Used for Testing ---- ###
@@ -225,29 +226,39 @@ if __name__ == '__main__':
     sl = SimulatedLidar(mymap, 100, 10000)
     translated_rotated_points, line_segment_eps, map_points, angles, r_dists, unrotated_points, r_angles_local = sl.simulate_lidar(loc=state)
     # Format Lidar Readings:
-    print(translated_rotated_points.shape, line_segment_eps.shape, map_points.shape)
-    print(angles.shape, r_dists.shape)
-    scan = np.stack((angles, r_dists), axis=1)
-    scan_v2 = np.stack((r_angles_local, r_dists), axis=1)
-    print(np.rad2deg(angles))
-    print(np.rad2deg(r_angles_local) % 360)
-    # exit()
-    print("Scan Shapes")
-    print(scan.shape, scan_v2.shape)
+
+    # print(translated_rotated_points.shape, line_segment_eps.shape, map_points.shape)
+    # print(angles.shape, r_dists.shape)
+    # scan = np.stack((angles, r_dists), axis=1)
+    # scan_v2 = np.stack((r_angles_local, r_dists), axis=1)
+    # print(np.rad2deg(angles))
+    # print(np.rad2deg(r_angles_local) % 360)
+    # # exit()
+    # print("Scan Shapes")
+    # print(scan.shape, scan_v2.shape)
 
 
     pf = ParticleFilter(mymap)
     pf._compute_dist_map()
+    pf.generate_initial_particles(num_particles=1000)
 
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    pf.visualize_map(ax1)
-    pf.visualize_dist_map(ax2)
-    plt.show()
+    # fig, (ax1, ax2) = plt.subplots(1, 2)
+    # pf.visualize_map(ax1)
+    # pf.visualize_dist_map(ax2)
+    # plt.show()
     
     
-    batch_simulated_lidar_readings, batch_probs, batch_normalized_weights =\
-        pf.batch_get_measurement_update(state.reshape(-1, 3), scan_v2, translated_rotated_points)
-    print(batch_probs)
+    # batch_simulated_lidar_readings, batch_probs, batch_normalized_weights =\
+    #     pf.batch_get_measurement_update(state.reshape(-1, 3), scan_v2, translated_rotated_points)
+    # print(batch_probs)
+
+    motion_delta = np.array([-100, 100, 0.0])
+    new_state = np.array([-1100.0, 2600, np.pi/2])
+    translated_rotated_points, line_segment_eps, map_points, angles, r_dists, unrotated_points, r_angles_local = sl.simulate_lidar(loc=new_state)
+    scan_v2 = np.stack((r_angles_local, r_dists), axis=1)
+    state_estimate = pf.step(motion_delta=motion_delta, scan=scan_v2)
+    print(f"State Estiamte: {np.round(state_estimate, 2)}")
+    
 
 
     
