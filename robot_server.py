a8da306a0da8e0b541080c3fd213a57d4e39b1d3
pmvt19 <praval.telagi@gmail.com>
2025-10-30 19:58:43 -0700

    Finished implementing robot server

--- robot_server.py
+++ robot_server.py
@@ -13,33 +13,55 @@ from robot_interface import RobotInterface
 import grpc
 import generated.robot_data_pb2 as pb2
 import generated.robot_data_pb2 as pb2_grpc
+import numpy as np
+import redis
 
 class SendingDataServicer(pb2_grpc.SendingDataServicer):
     """The server implementation of the SendingData service."""
     def __init__(self, device_name='/dev/tty.usbserial-FTAKRMAJ'):
         controller = DynamixelController(device_name=device_name, motor_ids=[1, 2])
         self.ri = RobotInterface(controller=controller)
+        self.redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
     def GetLatestLidarData(self, request, context):
         """Implements the RPC method."""
         print(f"Server received request: success={request.success}, message='{request.message}'")
 
+        # TODO: Not Required, Request is not an input that matters
         if not request.success:
             # You can set an error status if the request indicates failure
             context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
             context.set_details(request.message)
-            return pb2.FakeData() # Return an empty response
+            return pb2.LidarData() # Return an empty response
+        
+        lidar_data = np.frombuffer(self.redis_client.get("lidar_data")).reshape(-1, 2)
+
+        angles = lidar_data[:, 0]
+        dists = lidar_data[:, 1]
 
         # 1. Create the data to send back
-        fake_data = pb2.FakeData(
-            angles=[10.5, 20.1, 30.9, 40.2],
-            dists=[5.0, 10.1, 15.3, 20.7]
+        fake_data = pb2.LidarData(
+            angles=angles,
+            dists=dists
         )
 
         # 2. Return the data
         return fake_data
     
-    def SendMotionCommand(self, request, context):
-        pass
+    def SendMotionCommand(self, command, context):
+        motion_type = command.motion_type
+        dist = command.distance
+
+        if motion_type == 'linear':
+            m = self.ri.move_dist(dist)
+        elif motion_type == 'angular':
+            dist = np.deg2rad(dist)
+            m = self.ri.rotate_rad(dist)
+        
+        motion_distance = pb2.MotionDistance(
+            left_wheel_dist  =   m[0],
+            right_wheel_dist =   m[1]
+        )
+        return motion_distance
 
 
