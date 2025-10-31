4fc7dff4fd2ae820906bfdbf49fe49549c162f9b
pmvt19 <praval.telagi@gmail.com>
2025-10-30 18:59:56 -0700

    Commiting robot_server.py file which should run on the local robot

---
+++ robot_server.py
@@ -0,0 +1,11 @@
+# For GetLatestLidarData 
+
+# 1. Run Redis Lidar Publisher
+# 2. Read Latest Lidar Redis Info and Publish it if GetLatestLidarData is called
+
+# For SendMotionCommand
+# 1. Robot Will Use RobotInterface to move the robot according to the command
+# 2. Robot will send the distance of its wheel motion back to the laptop
+
+
+
