c11e28b0eacc3057dae4ad56ac57c3684c427115
pmvt19 <praval.telagi@gmail.com>
2025-10-31 23:33:31 -0700

    Slam algorithm for robot

---
+++ run_interactive_robot.py
@@ -0,0 +1,76 @@
+from robot import Robot
+from basic_map import BasicMap
+from map import Map
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import time
+
+
+# TODO: Rename this file and function?
+
+def run_interactive_robot():
+    pass
+
+# TODO BUG HACK FIXME: RERUN THIS SCRIPT!!!
+
+if __name__ == "__main__":
+
+
+    start_time = time.time()
+
+    # Initialization
+    robot = Robot(connection='client')
+    
+    scan, _ = robot.read_lidar_updated(manual_verification=True, wait_for_updated_reading=True)
+
+    map = Map(initial_scan=scan)
+
+    map.visualize(ax=plt.gca())
+    plt.show()
+
+    # for motion_type, dist in motions:
+    i=0
+    while True:
+        command = input("Enter Robot Motion Command")
+
+        if command == "quit":
+            break
+
+        motion_type, dist = command.split(",")
+        dist = float(dist)
+        print(motion_type, dist)
+        # continue
+        m = robot.command_motion_trial([motion_type, dist])
+        print(robot.state)
+        
+        predicted_state = robot.predict_state(robot.state, m)
+        print(robot.state)
+        print(f"Predicted State: {predicted_state}")
+        robot.state = predicted_state
+        # scan = robot.read_lidar()
+        print(robot.state)
+        scan, _ = robot.read_lidar_updated(manual_verification=True, wait_for_updated_reading=True)
+        print(f"Scan Size: {scan.shape}")
+        updated_state = map.update(scan, predicted_state)
+        print(robot.state)
+        print(f"Updated State: {updated_state}")
+        robot.state = updated_state
+
+        map.visualize(ax=plt.gca())
+        plt.show()
+        plt.cla()
+        map.visualize(ax=plt.gca())
+        plt.savefig(f'maps_saved/run0/map_{i}.png')
+        i+=1
+
+        pickle.dump(map, open("dumps/map_object.pickle", "wb"))
+        pickle.dump(map.map, open("dumps/map_map.pickle", "wb"))
+        pickle.dump(map.get_points(), open("dumps/map_points.pickle", "wb"))
+    
+    print("Finished")
+    plt.cla()
+    map.visualize(ax=plt.gca())
+    plt.savefig("generated_map.png")
+
+    
