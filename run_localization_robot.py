ddee9a506ac04d414a0b629a6db76e779e1ec1b6
pmvt19 <praval.telagi@gmail.com>
2025-10-31 23:34:01 -0700

    Particle Filter based localization for robot

---
+++ run_localization_robot.py
@@ -0,0 +1,78 @@
+from robot import Robot
+from map import Map
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from particle_filter import ParticleFilter
+import time
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+
+    # Initialization
+    robot = Robot(connection='client')
+    time.sleep(1.0)
+    
+    # Load Premade Map
+    map : Map = pickle.load(open("dumps/map_object.pickle", "rb"))
+
+    # Create Particle Filter Object
+    pf = ParticleFilter(map_obj=map)
+    pf._compute_dist_map()
+    pf.generate_initial_particles(num_particles=10000)
+
+    # Visualize Current Map
+    pf.visualize_particles(plt.gca())
+    pf.map.visualize_points(plt.gca())
+    plt.scatter(robot.state[0], robot.state[1], color='orange', zorder=2)
+    plt.show()
+
+    map.visualize(ax=plt.gca())
+    plt.show()
+
+    # for motion_type, dist in motions:
+    while True:
+        command = input("Enter Robot Motion Command")
+
+        if command == "quit":
+            break
+
+        motion_type, dist = command.split(",")
+        dist = float(dist)
+        print(motion_type, dist)
+        m = robot.command_motion_trial([motion_type, dist])
+        
+        predicted_state = robot.predict_state(robot.state, m)
+        # print(f"State Derivative: {dx_state}")
+        print(f"Predicted State: {predicted_state}")
+        robot.state = predicted_state
+        # scan = robot.read_lidar()
+        scan, lidar_data = robot.read_lidar_updated()
+
+        ## TODO: CLEAN THIS HACK
+        lidar_data = np.copy(lidar_data)
+        lidar_data[:, 0] = 360 - lidar_data[:, 0]
+        lidar_data[:, 0] = lidar_data[:, 0] + 90
+        lidar_data[:, 0] = lidar_data[:, 0] % 360
+        lidar_data[:, 0] = np.deg2rad(lidar_data[:, 0])
+        ## TODO: CLEAN THIS HACK
+
+        print(f"Scan Size: {scan.shape} | Lidar Data Size: {lidar_data.shape}")
+
+        updated_state = pf.step(motion_delta=[motion_type, dist], scan=lidar_data) # SCAN IS CURRENTLY WRONG!!!!
+        print(f"Updated State: {updated_state}")
+        robot.state = updated_state
+
+        pf.visualize_particles(plt.gca())
+        pf.map.visualize_points(plt.gca())
+        # plt.scatter(scan[:, 0], scan[:, 1], color='purple')
+        plt.scatter(robot.state[0], robot.state[1], color='orange', zorder=2)
+        plt.show()
+    
+    
+    print("Finished")
+
+
+
+    
