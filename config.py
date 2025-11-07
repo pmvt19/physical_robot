91f8d7ec602eadfe89748ca6f816d8e8cb5a4f3b
pmvt19 <praval.telagi@gmail.com>
2025-11-06 18:03:33 -0800

    File to house configurations for robot

---
+++ config.py
@@ -0,0 +1,25 @@
+# class RobotConfig():
+#     def __init__(self):
+#         pass
+
+parameters_dict = {
+    'simulated' : {
+        'lidar_map_path' : None
+    },
+    'client' : {
+        'channel_address': '192.168.12.155:50051'
+    },
+    'physical' : {
+        'lidar_port' : '/dev/ttyUSB0', # Linux
+        'dxl_motor_port' : '/dev/ttyUSB1', # Linux
+        'redis_host': 'localhost',
+        'redis_port': 6379
+    }
+}
+connection_type = 'client'
+
+config = {
+    'connection_type': connection_type,
+    connection_type : parameters_dict[connection_type]
+}
+
