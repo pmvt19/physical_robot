7149b6ddc05b46f5da320f81d72c5c135dd5e420
pmvt19 <praval.telagi@gmail.com>
2025-12-09 17:09:53 -0800

    Adding file with predefined prompts for vlm queries

---
+++ prompts.py
@@ -0,0 +1,9 @@
+ASSIGN_ROOM_LABEL_ONLY_PROMPT = \
+f"""
+You are an image analysis expert.
+
+Identify what kind of room the inputed image depicts.
+
+Please answer with only the room (can be one or multiple words).
+"""
+
