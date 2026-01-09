ef366858030099bef30da42a2ff869f5efa37b9a
pmvt19 <praval.telagi@gmail.com>
2026-01-09 14:03:29 -0800

    Enforcing output of vlm client for user command inputs from natural language

--- vlm_client.py
+++ vlm_client.py
@@ -10,6 +10,8 @@ import numpy as np
 from PIL import Image
 import io
 
+from vlm_output_schema import UserSemanticTarget
+
 class VLMClient():
     def __init__(self, model_id="gemini-robotics-er-1.5-preview"):
         # Load API Key from .env
@@ -18,6 +20,7 @@ class VLMClient():
 
         self.model_options = ["gemini-robotics-er-1.5-preview", "gemini-2.5-flash"]
         self.model_id = model_id
+        # self.model_id = "gemini-2.5-flash"
         self.client = genai.Client(api_key=GEMINI_API_KEY)
 
     def image_text_query(self, image_prompt : np.ndarray, text_prompt : str):
@@ -34,6 +37,7 @@ class VLMClient():
         # Get the bytes object
         image_bytes = byte_arr.getvalue()
 
+        # TODO: THIS IS BAD FIX THIS
         try:
             image_response = self.client.models.generate_content(
                 model=self.model_id,
@@ -60,8 +64,25 @@ class VLMClient():
                 text_prompt
             ],
             config = types.GenerateContentConfig(
+                response_mime_type = "application/json",
                 temperature=0.5,
-                thinking_config=types.ThinkingConfig(thinking_budget=0)
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+                response_json_schema=UserSemanticTarget.model_json_schema()
+            )
+        )
+        return text_response
+    
+    def text_query_output_schema(self, text_prompt : str, schema):
+        text_response = self.client.models.generate_content(
+            model=self.model_id,
+            contents=[
+                text_prompt
+            ],
+            config = types.GenerateContentConfig(
+                response_mime_type = "application/json",
+                temperature=0.5,
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+                response_json_schema=schema
             )
         )
         return text_response
