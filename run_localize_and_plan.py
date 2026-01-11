1f30c07f07f761cf12852a119f39f2d19dbb2caf
pmvt19 <praval.telagi@gmail.com>
2026-01-11 14:51:12 -0800

    Refactoring to use robot_utils

--- run_localize_and_plan.py
+++ run_localize_and_plan.py
@@ -20,16 +20,7 @@ from particle_filter import ParticleFilter
 
 from robot_utils import localize_robot, mpc_plan_and_follow_trajectory
 
-def is_close(state1, state2, threshold=100):
-    return np.linalg.norm(state1[:2] - state2[:2]) < threshold
-
-def run_motion_planning():
-    # Load Advanced Map
-    mymap = load_saved_advanced_map(directory='saves/scenes/extensive_apartment')
-    
-    # Initialize Robot Space
-    robot = PhysicalRobotSpace(mymap)
-
+def run_motion_planning(robot: PhysicalRobotSpace, target: NumpyState):
     # Localize Robot within Map
     start, pf = localize_robot(robot, robot.map)
 
@@ -37,8 +28,7 @@ def run_motion_planning():
     prm = PRM(env=robot, num_samples=10000, num_neighbors=10, validate_edges=True)
     prm.create_graph()
 
-    # TODO: REMOVE THE HARDCODED TARGET
-    target = robot.make_state(np.array([-1424.0, 656.0, 0.0])) # Left Side of Desk
+    # Plan and Follow path with MPC
     mpc_plan_and_follow_trajectory(robot, pf, robot.map, prm, robot.make_state(start), target)
 
 if __name__ == '__main__':
@@ -46,10 +36,17 @@ if __name__ == '__main__':
     print(f"Using Seed: {seed}")
     np.random.seed(seed)
 
-    run_motion_planning()
+    # Load Advanced Map
+    mymap = load_saved_advanced_map(directory='saves/scenes/extensive_apartment')
+    
+    # Initialize Robot Space
+    robot = PhysicalRobotSpace(mymap)
 
+    # List of Available Targets for current Map <- REMOVE with reconstructed map
     # target = robot.make_state(np.array([-1664.0, -1584.0, 0.0])) # Inside Bedroom
     # target = robot.make_state(np.array([2016.0, -834.0, 0.0])) # In Front of Bathroom
     # target = robot.make_state(np.array([-1424.0, 656.0, 0.0])) # Left Side of Desk
     # target = robot.make_state(np.array([190.0, 2229.0, 0.0])) # Next to Desk
-    # localize_mpc_planning(robot, target)
+
+    target = robot.make_state(np.array([-1424.0, 656.0, 0.0])) # Left Side of Desk
+    run_motion_planning(robot, target)
