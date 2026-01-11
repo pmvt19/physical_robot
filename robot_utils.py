d5bf08161ec35e9710c1082e18ab5974592af608
pmvt19 <praval.telagi@gmail.com>
2026-01-11 14:48:45 -0800

    Fixing types and adding loop stop in mpc function

--- robot_utils.py
+++ robot_utils.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 
 from particle_filter import ParticleFilter
 from robot import Robot
+from robot_space import PhysicalRobotSpace
 from map import Map
 from icp import run_icp
 from utils import transformation_mat_to_state
 
 from motion_planning.prm import PRM
+from motion_planning.state import NumpyState
 
 def is_close(state1, state2, threshold=100):
     return np.linalg.norm(state1[:2] - state2[:2]) < threshold
@@ -58,33 +60,31 @@ def localize_robot(robot: Robot,
         plt.show()
     return updated_state, pf
 
-def mpc_plan_and_follow_trajectory(robot: Robot,
+def mpc_plan_and_follow_trajectory(robot: PhysicalRobotSpace,
                                    pf: ParticleFilter,
                                    map: Map,
                                    prm: PRM,
-                                   start: np.ndarray,
-                                   target: np.ndarray):
+                                   start: NumpyState,
+                                   target: NumpyState):
     
     path = prm.search(start, target)
     path = [p.value for p in path]
     current_state = start
 
-    while True: # Fix this should stop eventually
+    while not is_close(current_state.value, target.value):
         motion_commands = robot.path_to_motion_commands(path)
         
         for i, motion_command in enumerate(motion_commands):
             print(f"Executing Motion Command: {motion_command}")
             m, predicted_state = robot.command_motion_and_predict_state(current_state.value, motion_command)
             
-            coords, lidar_data = robot.read_lidar_updated(wait_for_updated_reading=True, manual_verification=False)
+            coords, lidar_data = robot.read_lidar_updated(wait_for_updated_reading=True, manual_verification=True)
 
             # Particle Filter State Prediction Update
             current_state = pf.step(motion_delta=motion_command, scan=lidar_data)
             print(f"Current State After Particle Filter Update: {current_state}")
 
             # ICP State Refinement
-            # raise ValueError("CHECK IF WORKS FIRST")
-            print("Check if works first" * 100)
             T = run_icp(coords, map.get_points(), current_state, filter_init_outliers=False, visualize=True)
             current_state = robot.make_state(transformation_mat_to_state(T))
             print(f"Current State After Refinement: {current_state}")
