72aa56092ca80d8f1ee53423d6b1a0df9334e8cf
pmvt19 <praval.telagi@gmail.com>
2026-01-17 18:05:18 -0800

    Making vlm client robust to api call failures

--- vlm_client.py
+++ vlm_client.py
@@ -19,14 +19,35 @@ class VLMClient():
         GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
         self.model_options = ["gemini-robotics-er-1.5-preview", "gemini-2.5-flash"]
+        self.current_model_options_idx = 0
         self.model_id = model_id
-        # self.model_id = "gemini-2.5-flash"
         self.client = genai.Client(api_key=GEMINI_API_KEY)
 
-    def image_text_query(self, image_prompt : np.ndarray, text_prompt : str):
+    def _get_model_id(self):
+        new_model_id = self.model_options[self.current_model_options_idx % len(self.model_options)]
+        print(f"Using Model: {new_model_id}")
+        return new_model_id
+    
+    def protect_failed_api_calls(base_fn):
+        def enhanced_fn(*args, **kwargs):
+            while True:
+                try:
+                    return base_fn(*args, **kwargs)
+                except:
+                    user_input = input("Call to VLM Returned an Exception. Try Again? Or Swap Models? Or Quit?")
+                    if user_input.lower() == 'try again':
+                        continue
+                    elif user_input.lower() == 'swap models':
+                        args[0].current_model_options_idx += 1
+                    else:
+                        print("Returning `None`")
+                        break
+            return None
+        return enhanced_fn
 
+    def _get_image_bytes(self, image: np.ndarray):
         # Convert the NumPy array to a PIL Image object
-        pil_image = Image.fromarray(image_prompt)
+        pil_image = Image.fromarray(image)
 
         # Create an in-memory binary stream
         byte_arr = io.BytesIO()
@@ -37,69 +58,58 @@ class VLMClient():
         # Get the bytes object
         image_bytes = byte_arr.getvalue()
 
-        # # TODO: THIS IS BAD FIX THIS
-        # try:
-        #     image_response = self.client.models.generate_content(
-        #         model=self.model_id,
-        #         contents=[
-        #             types.Part.from_bytes(
-        #                 data=image_bytes,
-        #                 mime_type='image/png',
-        #             ),
-        #             text_prompt
-        #         ],
-        #         config = types.GenerateContentConfig(
-        #             temperature=0.5,
-        #             thinking_config=types.ThinkingConfig(thinking_budget=0)
-        #         )
-        #     )
-        # except:
-        #     image_response = None # TODO: Understand what to do here
-        # return image_response
-        image_response = None
-        while True:
-            try:
-                image_response = self.client.models.generate_content(
-                    model=self.model_id,
-                    contents=[
-                        types.Part.from_bytes(
-                            data=image_bytes,
-                            mime_type='image/png',
-                        ),
-                        text_prompt
-                    ],
-                    config = types.GenerateContentConfig(
-                        temperature=0.5,
-                        thinking_config=types.ThinkingConfig(thinking_budget=0)
-                    )
-                )
-            except:
-                user_input = input("Call to VLM Returned an Exception. Try Again?")
-                if user_input.lower() == 'yes':
-                    continue
-                else:
-                    print("Returning `None`")
-                    break
-        return image_response
+        return image_bytes
 
-    def text_query(self, text_prompt : str):
-        text_response = self.client.models.generate_content(
-            model=self.model_id,
+    def _image_text_query_schema(self, image_prompt: np.ndarray, text_prompt: str, schema: dict):
+        
+        image_response = self.client.models.generate_content(
+            model=self._get_model_id(),
             contents=[
+                types.Part.from_bytes(
+                    data=self._get_image_bytes(image_prompt),
+                    mime_type='image/png',
+                ),
                 text_prompt
             ],
             config = types.GenerateContentConfig(
-                response_mime_type = "application/json",
+                response_mime_type="application/json",
                 temperature=0.5,
                 thinking_config=types.ThinkingConfig(thinking_budget=0),
-                response_json_schema=UserSemanticTarget.model_json_schema()
+                response_json_schema=schema
             )
         )
-        return text_response
+        return image_response
     
-    def text_query_output_schema(self, text_prompt : str, schema):
+    def _image_text_query_no_schema(self, image_prompt: np.ndarray, text_prompt: str):
+
+        image_response = self.client.models.generate_content(
+            model=self._get_model_id(),
+            contents=[
+                types.Part.from_bytes(
+                    data=self._get_image_bytes(image_prompt),
+                    mime_type='image/png',
+                ),
+                text_prompt
+            ],
+            config = types.GenerateContentConfig(
+                temperature=0.5,
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+            )
+        )
+        return image_response
+    
+    @protect_failed_api_calls
+    def image_text_query(self, image_prompt: np.ndarray, text_prompt: str, schema: dict = None):
+        if schema is not None:
+            # Query Using Schema
+            return self._image_text_query_schema(image_prompt=image_prompt, text_prompt=text_prompt, schema=schema)
+        else:
+            # Query Using No Schema
+            return self._image_text_query_no_schema(image_prompt=image_prompt, text_prompt=text_prompt)
+    
+    def _text_query_schmea(self, text_prompt: str, schema: dict):
         text_response = self.client.models.generate_content(
-            model=self.model_id,
+            model=self._get_model_id(),
             contents=[
                 text_prompt
             ],
@@ -111,12 +121,34 @@ class VLMClient():
             )
         )
         return text_response
+    
+    def _text_query_no_schmea(self, text_prompt: str):
+        text_response = self.client.models.generate_content(
+            model=self._get_model_id(),
+            contents=[
+                text_prompt
+            ],
+            config = types.GenerateContentConfig(
+                temperature=0.5,
+                thinking_config=types.ThinkingConfig(thinking_budget=0),
+            )
+        )
+        return text_response
+    
+    @protect_failed_api_calls
+    def text_query(self, text_prompt: str, schema: dict = None):
+        if schema is not None:
+            # Query Using Schema
+            return self._text_query_schmea(text_prompt=text_prompt, schema=schema)
+        else:
+            # Query Using No Schema
+            return self._text_query_no_schmea(text_prompt=text_prompt)
 
 if __name__ == '__main__':
     vlm_client = VLMClient()
 
     # TODO: Change this file path
-    img = Image.open('/Users/pravaltelagi/oakd_camera/saves/apartment/rgb_imgs/0008.png')
+    img = Image.open('/Users/pravaltelagi/oakd_camera/saves/apartment/rgb_imgs/0004.png')
     img = np.asarray(img)
     response = vlm_client.image_text_query(img, ASSIGN_ROOM_LABEL_ONLY_PROMPT)
     print(response.text)
