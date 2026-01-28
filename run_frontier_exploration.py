2014efaf24d8cead9038d1940019252bd20fc4e9
pmvt19 <praval.telagi@gmail.com>
2026-01-27 16:41:05 -0800

    Working frontier exploration algorithm in trial

--- run_frontier_exploration.py
+++ run_frontier_exploration.py
@@ -13,6 +13,7 @@ from image_segmentation import ImageSegmenter
 from prompts import ASSIGN_ROOM_LABEL_ONLY_PROMPT, ASSIGN_ROOM_LABEL
 from vlm_output_schema import RoomLabel
 from sklearn.neighbors import KDTree
+from heapq import *
 
 def get_frontier_target(frontiers, advanced_map_state):
     kd_tree = KDTree(data=frontiers)
@@ -61,7 +62,48 @@ def bfs(start, target, grid):
     
     return path[::-1]
     
+def dijkstra(start, target, grid):
+    N, M = grid.shape
 
+    sx, sy = start
+    q = []
+    heappush(q, ((0.0, None, (sx, sy))))
+
+    visited = set()
+
+    neighbors = [(0,1), (1,0), (0,-1), (-1,0)]
+
+    child_to_parent = {(sx, sy) : None}
+
+    while q:
+        cost, parent, (x, y) = heappop(q)
+
+        if (x, y) in visited:
+            continue
+        visited.add((x,y))
+        child_to_parent[(x,y)] = parent
+
+        if (x, y) == (target[0], target[1]):
+            print("found_target")
+            break
+
+        for ox, oy in neighbors:
+            nx = x + ox
+            ny = y + oy
+
+            if nx >= 0 and nx < N and ny >= 0 and ny < M and grid[nx, ny] <= 0.65:
+                heappush(q, ((cost + grid[nx, ny], (x, y), (nx, ny))))
+                
+
+    target_tuple = (target[0], target[1])
+
+    current = target_tuple
+    path = []
+    while current:
+        path.append(current)
+        current = child_to_parent[current]
+    
+    return path[::-1]
         
 
 
@@ -71,20 +113,26 @@ def get_path_to_frontier(map: AdvancedMap, robot_state: np.ndarray, frontier_tar
     assert robot_state.shape[0] == 2
     assert frontier_target.shape[0] == 2
 
-    map_2d = map.get_map_2d()
+    map_2d = map.get_inflated_map_2d()
 
     grid_robot_state = map.world_to_grid_coords(robot_state[:2])
     grid_frontier_target = map.world_to_grid_coords(frontier_target)
 
-    path = bfs(grid_robot_state, grid_frontier_target, map_2d)
-    print(path)
+    path = dijkstra(grid_robot_state, grid_frontier_target, map_2d)
+    path = np.array(path)
+    return path
 
+def viz_map(map: AdvancedMap):
+    fig, ax = plt.subplots(1, 2)
+    map.visualize(ax[0])
+    map.visualize_points(ax[1])
+    plt.show()
 
 
 if __name__ == "__main__":
 
     ## TODO: Will update directory structure soon
-    scene_name = 'extensive_apartment'
+    scene_name = 'frontier_exploration_test'
     map_save_dir = f'saves/scenes/{scene_name}'
 
     # Initialization
@@ -101,31 +149,47 @@ if __name__ == "__main__":
     advanced_map.visualize(plt.gca())
     plt.show()
 
-    frontiers = advanced_map.get_frontiers()
-
     i=0
 
-    # Initialize Starting States for Each Map
+    # Initialize Starting State
     advanced_map_state = np.array([0.0, 0.0, 0.0])
 
     while True:
+        advanced_map.inflate_obstacles()
+        frontiers = advanced_map.get_frontiers()
         optimal_frontier = get_frontier_target(frontiers, advanced_map_state)
-        get_path_to_frontier(advanced_map, advanced_map_state[:2], optimal_frontier[0])
-
         print(f"Optimal Frontier: {optimal_frontier}")
-        advanced_map.get_frontiers()
-        
-        # Predict State for each Map
-        m = None
-        advanced_map_predicted_state = robot.predict_state(advanced_map_state, m)
 
-        # Read Lidar
-        scan, _ = robot.read_lidar_updated(manual_verification=True, wait_for_updated_reading=True)
+        path = get_path_to_frontier(advanced_map, advanced_map_state[:2], optimal_frontier[0])
+    
+        advanced_map.visualize_points(plt.gca())
+        plt.scatter(frontiers[:, 0], frontiers[:, 1])
 
-        # Update map and get updated state
-        advanced_map_updated_state = advanced_map.update(scan, advanced_map_predicted_state)
+        world_coord_path = advanced_map.batch_grid_to_approx_world_coords(path)
 
-        # Update the states for each map
-        advanced_map_state = advanced_map_updated_state
+        plt.scatter(world_coord_path[:, 0], world_coord_path[:, 1])
+        plt.show()
+
+        short_horizon_goal = world_coord_path[min(40, len(world_coord_path)-1)]
+
+        motion_commands = robot.path_to_motion_commands([advanced_map_state, np.array([short_horizon_goal[0], short_horizon_goal[1], 0.0])])
+        print(f"Motion Commands: {motion_commands[:2]}")
+        input("continue??") # TODO: Remove Later
+        for motion_command in motion_commands[:2]:
+            motion_type, motion_dist = motion_command
+            if abs(motion_dist) < 0.09:
+                print("Skipping Motion")
+                continue
+
+            m = robot.command_motion_trial(motion_command)
+            print(f"Returned m: {m}")
+            advanced_map_predicted_state = robot.predict_state(advanced_map_state, m)
+            scan, _ = robot.read_lidar_updated(manual_verification=True, wait_for_updated_reading=True)
+            advanced_map_updated_state = advanced_map.update(scan, advanced_map_predicted_state)
+            advanced_map_state = advanced_map_updated_state
+            viz_map(advanced_map)
 
         i += 1
+
+        # Save Map
+        advanced_map.save(map_save_dir, file_name_ext=f"step_{i}")
