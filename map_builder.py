bf8003fc828dd208450f7108dca7da6a8597f3f8
pmvt19 <praval.telagi@gmail.com>
2026-02-02 16:48:47 -0800

    Outlining map builder classes to unify map building code

---
+++ map_builder.py
@@ -0,0 +1,24 @@
+from map import Map
+from advanced_map import AdvancedMap
+from semantic_map import SemanticMap
+
+class MapBuilder():
+    def __init__(self):
+        pass
+    
+    def step(self):
+        pass
+
+class AdvancedMapBuilder(MapBuilder):
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
+
+class SemanticMapBuilder(MapBuilder):
+    def __init__(self):
+        pass
+
+    def step(self):
+        pass
