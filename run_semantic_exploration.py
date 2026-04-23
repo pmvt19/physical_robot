9fa8fdb8436a34dc5949f40677d64193f22f877e
pmvt19 <praval.telagi@gmail.com>
2026-04-22 17:04:43 -0700

    Planning semantic exploration

---
+++ run_semantic_exploration.py
@@ -0,0 +1,13 @@
+
+# Outline
+
+# 1. Get User Semantic Target
+
+# 2. Initialize Exploration Map In-Place (90 Degree Point Turns)
+
+# 3. Move toward closest matching semantics
+
+# 4. Repeat until object is found or no semantic frontiers
+
+if __name__ == '__main__':
+    pass
