b9dccada8d6348e1ddc0a14ed860dfd47af573d2
pmvt19 <praval.telagi@gmail.com>
2026-05-02 18:08:52 -0700

    Adding user post target schema

--- vlm_output_schema.py
+++ vlm_output_schema.py
@@ -6,5 +6,10 @@ class UserSemanticTarget(BaseModel):
     item_name: str
     reason: str
 
+class UserPoseTarget(BaseModel):
+    valid: bool
+    x: float
+    y: float
+
 class RoomLabel(BaseModel):
     room_label: str
