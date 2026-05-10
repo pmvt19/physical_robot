a1d119a7a2a6529028e2e0b67247bba571092626
pmvt19 <praval.telagi@gmail.com>
2026-05-09 19:49:18 -0700

    Updating navigation procedure

--- physical_robot/processes/run_navigation.py
+++ physical_robot/processes/run_navigation.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from physical_robot.robot import Robot
 from physical_robot.robot.robot_space import PhysicalRobotSpace
 from physical_robot.task_planner.task_planner import TaskPlanner
@@ -9,18 +12,21 @@ from motion_planning.search import RRT
 def run_navigation():
     # Initialize Objects
     robot = Robot(connection='client')
-    # mymap = load_saved_semantic_map("saves/scenes/extensive_apartment")
     mymap = load_saved_advanced_map('saves/scenes/refactor_test_map')
+
     task_planner = TaskPlanner(robot, mymap)
     robot_space = PhysicalRobotSpace(mymap)
     rrt = RRT(robot_space, delta=500.0)
     path_tracker = PathTracker(robot_space, rrt)
 
+    # Get the Target Pose
     target = task_planner.get_target_pose()
+    target = robot_space.make_state(target)
     
-    start, _ = path_tracker.localize_robot()
-    start = robot_space.make_state(start)
-
+    # Define Start
+    path_tracker.localize_robot()
+    start = robot_space.make_state(path_tracker.robot_location)
+    
     path = rrt.search(start, target)
     path = path_obj_to_list_of_states(path)
 
