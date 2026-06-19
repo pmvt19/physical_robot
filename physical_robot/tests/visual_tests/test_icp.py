19a424fb1425736007ce599c46ac4bf5606d2f36
pmvt19 <praval.telagi@gmail.com>
2026-06-19 16:50:13 -0700

    Adding unittests for icp

--- physical_robot/tests/visual_tests/test_icp.py
+++ physical_robot/tests/visual_tests/test_icp.py
@@ -1,12 +1,27 @@
 # TODO: This is a planned file for the visual tests for the ICP Function
+import unittest
+import numpy as np
 
+from physical_robot.algorithms.icp import run_icp
 # Note this file will not be considered a Unit test as we will only verify
 # correctness visually
 
 # Have sample data in the repo
 
 # TEST 1: ICP with only rotation
+class TestICP(unittest.TestCase):
+    def test_icp_rotation(self):
+        coords_0 = np.load("test_data/icp/icp_sample_data_rotation_0.npy")
+        state_0 = np.load("test_data/icp/state_rotation_0.npy")
+        coords_1 = np.load("test_data/icp/icp_sample_data_rotation_1.npy")
+        state_1 = np.load("test_data/icp/state_rotation_1.npy")
+
+        run_icp(coords_1, coords_0, state_1, visualize=True)
 
 # TEST 2: ICP with only translation
+    def test_icp_translation(self):
+        pass
 
 # TEST 3: ICP with rotation and translation
+    def test_icp_rotation_and_translation(self):
+        pass
