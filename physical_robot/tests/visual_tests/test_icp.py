2c4a540b9a04ae632a38ca529b2b12e9217566e0
pmvt19 <praval.telagi@gmail.com>
2026-06-19 16:59:44 -0700

    Adding unit tests for icp

--- physical_robot/tests/visual_tests/test_icp.py
+++ physical_robot/tests/visual_tests/test_icp.py
@@ -12,7 +12,6 @@ from physical_robot.algorithms.icp import run_icp
 class TestICP(unittest.TestCase):
     def test_icp_rotation(self):
         coords_0 = np.load("test_data/icp/icp_sample_data_rotation_0.npy")
-        state_0 = np.load("test_data/icp/state_rotation_0.npy")
         coords_1 = np.load("test_data/icp/icp_sample_data_rotation_1.npy")
         state_1 = np.load("test_data/icp/state_rotation_1.npy")
 
@@ -20,8 +19,16 @@ class TestICP(unittest.TestCase):
 
 # TEST 2: ICP with only translation
     def test_icp_translation(self):
-        pass
+        coords_0 = np.load("test_data/icp/icp_sample_data_translational_0.npy")
+        coords_1 = np.load("test_data/icp/icp_sample_data_translational_1.npy")
+        state_1 = np.load("test_data/icp/state_translational_1.npy")
+
+        run_icp(coords_1, coords_0, state_1, visualize=True)
 
 # TEST 3: ICP with rotation and translation
     def test_icp_rotation_and_translation(self):
-        pass
+        coords_0 = np.load("test_data/icp/icp_sample_data_rotation_and_translation_0.npy")
+        coords_4 = np.load("test_data/icp/icp_sample_data_rotation_and_translation_4.npy")
+        state_4 = np.load("test_data/icp/state_rotation_and_translation_4.npy")
+
+        run_icp(coords_4, coords_0, state_4, visualize=True)
