ca077c2fd9062870b0d6b7beef7e30c663c510ff
pmvt19 <praval.telagi@gmail.com>
2026-08-03 23:43:20 -0700

    Adding content to unit tests

--- physical_robot/tests/unit_tests/test_robot_controller.py
+++ physical_robot/tests/unit_tests/test_robot_controller.py
@@ -1,8 +1,20 @@
 import unittest
+import numpy as np
+
+from physical_robot.robot.robot_controller import RobotController
 
 # Testing Unittest
 
 class RobotControllerTests(unittest.TestCase):
     def test_unit_test(self):
-        pass
+        fake_path = [
+                np.array([0.0, 0.0, 0.0]),
+                np.array([0.0, 10.0, np.pi]),
+                np.array([10.0, 10.0, np.pi/2]),
+                np.array([10.0, 0.0, 0.0]),
+                np.array([0.0, 0.0, np.pi*1.5]),
+            ]
+        
+        controller = RobotController()
+        print(controller.compute_motion_commands(fake_path))
     
