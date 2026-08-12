85f06f1516c601901fabb3b761e2d64ab4f2b5d2
pmvt19 <praval.telagi@gmail.com>
2026-08-11 22:23:16 -0700

    Formating advanced map builder file

--- physical_robot/map_builder/advanced_map_builder.py
+++ physical_robot/map_builder/advanced_map_builder.py
@@ -1,6 +1,6 @@
-
 from physical_robot.map_builder.map_builder import MapBuilder
 from physical_robot.maps import AdvancedMap
+
 
 class AdvancedMapBuilder(MapBuilder):
     def __init__(self, robot, map_resolution=10.0, manual_lidar_verification=False):
