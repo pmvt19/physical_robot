1aa66e0c4682c089a0199ef09da37dbda3a2908e
pmvt19 <praval.telagi@gmail.com>
2026-08-23 11:13:03 -0700

    Converting vlm tests to unittest framework

--- physical_robot/tests/unit_tests/test_vlm_client.py
+++ physical_robot/tests/unit_tests/test_vlm_client.py
@@ -1,3 +1,5 @@
+import unittest
+
 import numpy as np
 
 from PIL import Image
@@ -5,85 +7,87 @@ from PIL import Image
 from physical_robot.models.vlm.vlm_client import VLMClient
 from physical_robot.models.vlm.prompts import EXTRACT_SEMANTIC_TARGETS, ASSIGN_ROOM_LABEL
 from physical_robot.models.vlm.vlm_output_schema import UserSemanticTarget, RoomLabel
+from physical_robot.models.vlm.gemma_vlm_client import OllamaVLMClient
 
-def test_text_prompt_no_schema():
-    vlm_client = VLMClient()
 
-    vlm_response = vlm_client.text_query("What is the room where you would want to cook your meals?")
-    assert 'kitchen' in vlm_response
+class TestVLMClients(unittest.TestCase):
+    def setUp(self):
+        self.vlm_client = OllamaVLMClient()
 
-def test_text_prompt_schema():
-    vlm_client = VLMClient()
+        self.room_list = ['bedroom', 'kitchen', 'office']
+        self.object_list = ['bed', 'oven', 'desk', 'chair']
+        self.invalid_object_list = ['person', 'sky']
+        
+    def test_text_prompt_no_schema(self):
+        vlm_response = self.vlm_client.text_query("What is the room where you would cook your meals?")
+        self.assertIn("kitchen", vlm_response)
 
-    room_list = ['bedroom', 'kitchen', 'office']
-    object_list = ['bed', 'oven', 'desk', 'chair']
-    invalid_object_list = ['person', 'sky']
+    def test_text_prompt_schema_valid_room(self):
+        vlm_response = self.vlm_client.text_query(
+            EXTRACT_SEMANTIC_TARGETS.format(
+                "Let's go towards the kitchen", 
+                self.room_list, self.object_list, self.invalid_object_list
+            ),
+            UserSemanticTarget.model_json_schema()
+        )
 
-    vlm_response = vlm_client.text_query(EXTRACT_SEMANTIC_TARGETS.format("Let's go towards the kitchen", 
-                                                                             room_list,
-                                                                             object_list,
-                                                                             invalid_object_list),
-                                        UserSemanticTarget.model_json_schema())
+        user_semantic_target = UserSemanticTarget.model_validate_json(vlm_response)
 
-    user_semantic_target = UserSemanticTarget.model_validate_json(vlm_response)
-    assert user_semantic_target.semantic_level == 'room'
-    assert user_semantic_target.valid == True
-    assert user_semantic_target.item_name == "kitchen"
-
-    vlm_response = vlm_client.text_query(EXTRACT_SEMANTIC_TARGETS.format("Let's go towards the desk", 
-                                                                             room_list,
-                                                                             object_list,
-                                                                             invalid_object_list),
-                                        UserSemanticTarget.model_json_schema())
-
-    user_semantic_target = UserSemanticTarget.model_validate_json(vlm_response)
-    assert user_semantic_target.semantic_level == 'object'
-    assert user_semantic_target.valid == True
-    assert user_semantic_target.item_name == "desk"
-
-    vlm_response = vlm_client.text_query(EXTRACT_SEMANTIC_TARGETS.format("DKJKLSDFKJL", 
-                                                                             room_list,
-                                                                             object_list,
-                                                                             invalid_object_list),
-                                        UserSemanticTarget.model_json_schema())
-
-    user_semantic_target = UserSemanticTarget.model_validate_json(vlm_response)
+        self.assertEqual(user_semantic_target.semantic_level, "room")
+        self.assertTrue(user_semantic_target.valid)
+        self.assertEqual(user_semantic_target.item_name, "kitchen")
     
-    assert user_semantic_target.valid == False
-    assert user_semantic_target.semantic_level == 'N/A'
-    assert user_semantic_target.item_name == "N/A"
+    def test_text_prompt_schema_valid_object(self):
+        vlm_response = self.vlm_client.text_query(
+            EXTRACT_SEMANTIC_TARGETS.format(
+                "Let's go towards the desk", self.room_list,
+                self.object_list, self.invalid_object_list
+            ),
+            UserSemanticTarget.model_json_schema()
+        )
 
-def test_image_text_prompt_no_schema():
-    vlm_client = VLMClient()
+        user_semantic_target = UserSemanticTarget.model_validate_json(vlm_response)
 
-    img = Image.open('/Users/pravaltelagi/oakd_camera/saves/apartment/rgb_imgs/0004.png')
-    img = np.asarray(img)
-    vlm_response = vlm_client.image_text_query(img, "What is this an image of?")
+        self.assertEqual(user_semantic_target.semantic_level, "object")
+        self.assertTrue(user_semantic_target.valid)
+        self.assertEqual(user_semantic_target.item_name, "desk")
 
-    assert 'kitchen' in vlm_response
+    def test_text_prompt_schema_invalid_object(self):
+        vlm_response = self.vlm_client.text_query(
+            EXTRACT_SEMANTIC_TARGETS.format(
+                "DKJKLSDFKJL", self.room_list, self.object_list, self.invalid_object_list
+            ),
+            UserSemanticTarget.model_json_schema()
+        )
 
-def test_image_text_prompt_schema():
-    vlm_client = VLMClient()
+        user_semantic_target = UserSemanticTarget.model_validate_json(vlm_response)
+    
+        self.assertFalse(user_semantic_target.valid)
+        self.assertEqual(user_semantic_target.semantic_level, "N/A")
+        self.assertEqual(user_semantic_target.item_name, "N/A")
 
-    img = Image.open('/Users/pravaltelagi/oakd_camera/saves/apartment/rgb_imgs/0004.png')
-    img = np.asarray(img)
+    def test_image_text_prompt_no_schema(self):
+        img = Image.open('test_data/vlm_test_imgs/img0.png')
+        img = np.asarray(img)
+        vlm_response = self.vlm_client.image_text_query(img, "What is this an image of?")
 
-    existing_rooms = ['kitchen', 'office' 'bedroom']
-    invalid_rooms = ['wall', 'room']
+        self.assertIn("kitchen", vlm_response)
 
-    vlm_response = vlm_client.image_text_query(img, 
-                                               ASSIGN_ROOM_LABEL.format(
-                                                    existing_rooms,
-                                                    invalid_rooms
-                                                ), 
-                                                RoomLabel.model_json_schema())
+    def test_image_text_prompt_schema(self):
+        img = Image.open('test_data/vlm_test_imgs/img0.png')
+        img = np.asarray(img)
 
-    room_label = RoomLabel.model_validate_json(vlm_response)
+        existing_rooms = ['kitchen', 'office' 'bedroom']
+        invalid_rooms = ['wall', 'room']
 
-    assert room_label.room_label == 'kitchen'
+        vlm_response = self.vlm_client.image_text_query(
+            img, 
+            ASSIGN_ROOM_LABEL.format(
+                existing_rooms, invalid_rooms
+            ), 
+            RoomLabel.model_json_schema()
+        )
 
-if __name__ == '__main__':
-    test_text_prompt_no_schema()
-    test_text_prompt_schema()
-    test_image_text_prompt_no_schema()
-    test_image_text_prompt_schema()
+        room_label = RoomLabel.model_validate_json(vlm_response)
+
+        self.assertEqual(room_label.room_label, "kitchen")
