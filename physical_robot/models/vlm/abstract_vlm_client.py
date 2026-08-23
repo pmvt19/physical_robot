a883019e2a9e1e471407dbeaac18965d2c997e6e
pmvt19 <praval.telagi@gmail.com>
2026-08-23 08:58:54 -0700

    Adding logging to abstract vlm client

--- physical_robot/models/vlm/abstract_vlm_client.py
+++ physical_robot/models/vlm/abstract_vlm_client.py
@@ -1,6 +1,11 @@
 import numpy as np
 from PIL import Image
 import io
+import logging
+
+from physical_robot.utils.utils import register_logger
+
+logger = register_logger(logger_name=__name__, log_filename='vlm_client', level=logging.INFO, std_err=False)
 
 # TODO: Add Base Class??
 class AbstractVLMClient():
@@ -29,12 +34,18 @@ class AbstractVLMClient():
         raise NotImplementedError
     
     def image_text_query(self, image_prompt: np.ndarray, text_prompt: str, schema: dict = None):
+        output = None
         if schema is not None:
             # Query Using Schema
-            return self._image_text_query_schema(image_prompt=image_prompt, text_prompt=text_prompt, schema=schema)
+            output = self._image_text_query_schema(image_prompt=image_prompt, text_prompt=text_prompt, schema=schema)
         else:
             # Query Using No Schema
-            return self._image_text_query_no_schema(image_prompt=image_prompt, text_prompt=text_prompt)
+            output = self._image_text_query_no_schema(image_prompt=image_prompt, text_prompt=text_prompt)
+
+        logger.info(f"[Image Text Query] Text Prompt: {text_prompt}, Schema: {schema}, VLM Output: {output}")
+
+        return output
+        
 
     def _text_query_schmea(self, text_prompt: str, schema: dict):
         raise NotImplementedError
@@ -43,9 +54,14 @@ class AbstractVLMClient():
         raise NotImplementedError
 
     def text_query(self, text_prompt: str, schema: dict = None):
+        output = None
         if schema is not None:
             # Query Using Schema
-            return self._text_query_schmea(text_prompt=text_prompt, schema=schema)
+            output = self._text_query_schmea(text_prompt=text_prompt, schema=schema)
         else:
             # Query Using No Schema
-            return self._text_query_no_schmea(text_prompt=text_prompt)
+            output = self._text_query_no_schmea(text_prompt=text_prompt)
+
+        logger.info(f"[Text Query] Text Prompt: {text_prompt}, Schema: {schema}, VLM Output: {output}")
+
+        return output
