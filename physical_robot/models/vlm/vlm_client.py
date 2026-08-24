1673d1fd2d1325ac23625bcfb73ee2855b0ec747
pmvt19 <praval.telagi@gmail.com>
2026-08-23 22:31:16 -0700

    Formating vlm client file

--- physical_robot/models/vlm/vlm_client.py
+++ physical_robot/models/vlm/vlm_client.py
@@ -1,30 +1,35 @@
-from google import genai
-from google.genai import types
-
-import os 
-from dotenv import load_dotenv
-
-from physical_robot.models.vlm.prompts import ASSIGN_ROOM_LABEL_ONLY_PROMPT
+import os
 
 import numpy as np
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 from PIL import Image
-import io
 
 from physical_robot.models.vlm.abstract_vlm_client import AbstractVLMClient
+from physical_robot.models.vlm.prompts import ASSIGN_ROOM_LABEL_ONLY_PROMPT
+
 
 class VLMClient(AbstractVLMClient):
     def __init__(self, model_id="gemini-robotics-er-1.6-preview"):
         # Load API Key from .env
         load_dotenv()
-        GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+        GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-        self.model_options = ["gemini-robotics-er-1.6-preview", "gemini-2.5-flash", "gemini-2.5-pro", "gemini-2.0-flash", "gemini-2.0-flash-lite"]
+        self.model_options = [
+            "gemini-robotics-er-1.6-preview",
+            "gemini-2.5-flash",
+            "gemini-2.5-pro",
+            "gemini-2.0-flash",
+            "gemini-2.0-flash-lite"
+        ]
         self.current_model_options_idx = 0
         self.model_id = model_id
         self.client = genai.Client(api_key=GEMINI_API_KEY)
 
     def _get_model_id(self):
-        new_model_id = self.model_options[self.current_model_options_idx % len(self.model_options)]
+        new_model_idx = self.current_model_options_idx % len(self.model_options)
+        new_model_id = self.model_options[new_model_idx]
         print(f"Using Model: {new_model_id}")
         return new_model_id
     
@@ -34,7 +39,10 @@ class VLMClient(AbstractVLMClient):
                 try:
                     return base_fn(*args, **kwargs)
                 except:
-                    user_input = input("Call to VLM Returned an Exception. Try Again? Or Swap Models? Or Quit?")
+                    user_input = input(
+                        "Call to VLM Returned an Exception. "
+                        "Try Again? Or Swap Models? Or Quit?"
+                    )
                     if user_input.lower() == 'try again':
                         continue
                     elif user_input.lower() == 'swap models':
@@ -45,7 +53,9 @@ class VLMClient(AbstractVLMClient):
             return None
         return enhanced_fn
 
-    def _image_text_query_schema(self, image_prompt: np.ndarray, text_prompt: str, schema: dict):
+    def _image_text_query_schema(
+        self, image_prompt: np.ndarray, text_prompt: str, schema: dict
+    ):
         
         image_response = self.client.models.generate_content(
             model=self._get_model_id(),
@@ -66,7 +76,6 @@ class VLMClient(AbstractVLMClient):
         return image_response.text
     
     def _image_text_query_no_schema(self, image_prompt: np.ndarray, text_prompt: str):
-
         image_response = self.client.models.generate_content(
             model=self._get_model_id(),
             contents=[
@@ -115,7 +124,9 @@ if __name__ == '__main__':
     vlm_client = VLMClient()
 
     # TODO: Change this file path
-    img = Image.open('/Users/pravaltelagi/oakd_camera/saves/apartment/rgb_imgs/0004.png')
+    img = Image.open(
+        "/Users/pravaltelagi/oakd_camera/saves/apartment/rgb_imgs/0004.png"
+    )
     img = np.asarray(img)
     response = vlm_client.image_text_query(img, ASSIGN_ROOM_LABEL_ONLY_PROMPT)
     print(response)
