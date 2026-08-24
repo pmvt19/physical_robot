57983d64d2cf66e59006f37d4b99bc7fd62c62db
pmvt19 <praval.telagi@gmail.com>
2026-08-24 00:54:15 -0700

    Fixed camera monitoring to work with client

--- physical_robot/monitoring/viz_camera.py
+++ physical_robot/monitoring/viz_camera.py
@@ -1,27 +1,14 @@
 import cv2
-import numpy as np
-import redis
 
-redis_client = redis.Redis(host='localhost', port=6379, db=0)
+from physical_robot.robot import Robot 
+
+robot = Robot(connection='client')
 
 while True:
-    rgb_img_bytes = redis_client.get("rgb_img")
-    stereo_img_bytes = redis_client.get("stereo_img")
+    rgb_numpy, depth_numpy = robot.read_rgb_camera()
 
-    rgb_img_shape_bytes = redis_client.get("rgb_img_shape")
-    stereo_img_shape_bytes = redis_client.get("stereo_img_shape")
-
-    rgb_img_shape = np.frombuffer(rgb_img_shape_bytes, dtype=np.int64)
-    stereo_img_shape = np.frombuffer(stereo_img_shape_bytes, dtype=np.int64)
-
-    rgb_img = np.frombuffer(rgb_img_bytes, dtype=np.uint8).reshape(480, 640, 3)
-    stereo_img = np.frombuffer(stereo_img_bytes, dtype=np.int16).reshape(240, 320)
-
-    print("rgb", rgb_img.shape)
-    print("Stereo", stereo_img.shape)
-
-    cv2.imshow('frame', rgb_img)
-    cv2.imshow('depth frame', stereo_img)
+    cv2.imshow('frame', rgb_numpy)
+    cv2.imshow('depth frame', depth_numpy)
 
     if cv2.waitKey(1) == ord('q'):
         break
