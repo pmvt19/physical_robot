b00c4d4669af46611292f59a4112a67a06c1d712
pmvt19 <praval.telagi@gmail.com>
2026-08-24 21:23:48 -0700

    Fixing motor interfact formatting

--- physical_robot/hardware/motors/robot_motor_interface.py
+++ physical_robot/hardware/motors/robot_motor_interface.py
@@ -1,13 +1,22 @@
-from .dxl_controller import *
-import numpy as np
-import time
-from physical_robot.utils import register_logger
-
+import logging
 from threading import Lock
 
-logger = register_logger(logger_name=__name__, log_filename='robot_interface', level=logging.DEBUG)
+import numpy as np
 
-class MockLock():
+from physical_robot.hardware.motors.dxl_controller import (
+    EXTENDED_POSITION_CONTROL_MODE,
+    TORQUE_DISABLE,
+    TORQUE_ENABLE,
+    DynamixelController,
+)
+from physical_robot.utils import register_logger
+
+logger = register_logger(
+    logger_name=__name__, log_filename="robot_interface", level=logging.DEBUG
+)
+
+
+class MockLock:
     def __init__(self):
         pass
 
@@ -17,7 +26,8 @@ class MockLock():
     def release(self):
         pass
 
-class LoggedLock():
+
+class LoggedLock:
     def __init__(self):
         self.lock = Lock()
 
@@ -29,26 +39,18 @@ class LoggedLock():
         self.lock.release()
         logger.info("Releasing Lock")
 
-class RobotInterface():
+
+class RobotInterface:
     def __init__(self, controller):
-        self.controller : DynamixelController = controller
+        self.controller: DynamixelController = controller
 
         self.linear_velocity = 10
         self.controller_lock = Lock()
 
-        # self.controller.set_torque(1, TORQUE_DISABLE)
-        # self.controller.set_torque(2, TORQUE_DISABLE)
         self.set_torque(TORQUE_DISABLE)
 
-        # self.controller.set_operating_mode(1, VELOCITY_CONTROL_MODE)
-        # self.controller.set_operating_mode(2, VELOCITY_CONTROL_MODE)
-
-        # self.controller.set_operating_mode(1, EXTENDED_POSITION_CONTROL_MODE)
-        # self.controller.set_operating_mode(2, EXTENDED_POSITION_CONTROL_MODE)
         self.set_mode(EXTENDED_POSITION_CONTROL_MODE)
 
-        # self.controller.set_torque(1, TORQUE_ENABLE)
-        # self.controller.set_torque(2, TORQUE_ENABLE)
         self.set_torque(TORQUE_ENABLE)
 
         self.r = 66.5 / 2
@@ -74,7 +76,7 @@ class RobotInterface():
         self.controller.set_operating_mode(2, mode)
 
         self.controller_lock.release()
-    
+
     def set_profile_velocity(self, velocity=100):
         self.controller_lock.acquire()
 
@@ -91,7 +93,7 @@ class RobotInterface():
 
         self.controller_lock.release()
         return np.array([v1, v2])
-    
+
     def set_motor_positions(self, positions):
         self.controller_lock.acquire()
 
@@ -99,7 +101,7 @@ class RobotInterface():
         self.controller.set_position(id=2, position=positions[1])
 
         self.controller_lock.release()
-    
+
     def get_motor_positions(self):
         self.controller_lock.acquire()
 
@@ -108,7 +110,7 @@ class RobotInterface():
         self.controller_lock.release()
 
         return np.array([motor_id_1_pos, motor_id_2_pos], dtype=np.int64)
-        
+
     def move_forward(self):
         self.controller_lock.acquire()
 
@@ -132,7 +134,7 @@ class RobotInterface():
         self.controller.set_velocity(id=2, velocity_rpm=self.linear_velocity)
 
         self.controller_lock.release()
-    
+
     def rotate_left(self):
         self.controller_lock.acquire()
 
@@ -140,7 +142,7 @@ class RobotInterface():
         self.controller.set_velocity(id=2, velocity_rpm=-self.linear_velocity)
 
         self.controller_lock.release()
-    
+
     def stop_motion(self):
         self.controller_lock.acquire()
 
@@ -149,16 +151,9 @@ class RobotInterface():
 
         self.controller_lock.release()
 
-    
 
-
-
-if __name__ == '__main__':
-    controller = DynamixelController(device_name='/dev/tty.usbserial-FTAKRMAJ', motor_ids=[1, 2])
+if __name__ == "__main__":
+    controller = DynamixelController(
+        device_name="/dev/tty.usbserial-FTAKRMAJ", motor_ids=[1, 2]
+    )
     ri = RobotInterface(controller=controller)
-
-    
-    
-
-    
-
